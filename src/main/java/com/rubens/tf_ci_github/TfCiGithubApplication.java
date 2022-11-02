f3f0dc4cf9fcb2a9149194811a8fecd8302c0937
Rubens Rangel <rubens.rangel@b2wdigital.com>
2022-11-02 09:46:24 -0300

    Project Started

---
+++ src/main/java/com/rubens/tf_ci_github/TfCiGithubApplication.java
@@ -0,0 +1,13 @@
+package com.rubens.tf_ci_github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfCiGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfCiGithubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rubens/tf_ci_github/TfCiGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rubens.tf_ci_github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfCiGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
